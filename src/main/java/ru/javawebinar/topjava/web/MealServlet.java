cf1d0edf8feb0512915eb71963cc8372c2d00047
NicolasUA <nicolas.bakutkin@gmail.com>
2015-09-15 00:52:35 +0300

    CRUD for mealList

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,20 @@
 package ru.javawebinar.topjava.web;
 
 import ru.javawebinar.topjava.LoggerWrapper;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.util.UserMealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 /**
  * User: gkislin
@@ -14,11 +22,45 @@ import java.io.IOException;
  */
 public class MealServlet extends HttpServlet {
     private static final LoggerWrapper LOG = LoggerWrapper.get(MealServlet.class);
+    private static List<UserMeal> mealList = Arrays.asList(
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 500),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 1000),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+    );
+    private LinkedList<UserMeal> list = new LinkedList<>(mealList);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("redirect to mealList");
 
-//        request.getRequestDispatcher("/mealList.jsp").forward(request, response);
+        if ("delete".equals(request.getParameter("action"))) {
+            list.remove(Integer.parseInt(request.getParameter("id")));
+        }
+        UserMeal newMeal = null;
+        Integer id = null;
+        if ("edit".equals(request.getParameter("action"))) {
+            id = Integer.parseInt(request.getParameter("id"));
+            newMeal = list.get(id);
+        }
+        if ("add".equals(request.getParameter("action"))) {
+            newMeal = new UserMeal(LocalDateTime.parse(request.getParameter("dateTime"), DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                    request.getParameter("description"),
+                    Integer.parseInt(request.getParameter("calories")));
+            try {
+                list.remove(Integer.parseInt(request.getParameter("id")));
+            } catch (Exception e) {}
+            list.add(newMeal);
+            list.sort((o1, o2) -> o1.getDateTime().compareTo(o2.getDateTime()));
+            newMeal = null;
+            id = null;
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute("mealList", UserMealsUtil.getFilteredMealsWithExceeded(list, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000));
+        session.setAttribute("editMeal", newMeal);
+        session.setAttribute("id", id);
         response.sendRedirect("mealList.jsp");
     }
 }
--- src/main/webapp/mealList.jsp
+++ src/main/webapp/mealList.jsp
@@ -1,10 +1,58 @@
+<%@ page import="java.time.format.DateTimeFormatter" %>
+<%@ page import="ru.javawebinar.topjava.model.UserMealWithExceed" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+
 <html>
 <head>
     <title>Meal list</title>
 </head>
 <body>
-<h2>Meal list</h2>
-<h2>"<a href="/topjava">Домой"</a></h2>
+    <h2>Meal list</h2>
+    <table width="1000" class="pure-table pure-table-bordered">
+        <thead>
+            <tr align="center">
+                <th width="25%">Date</th>
+                <th width="40%">Description</th>
+                <th width="10%">Calories</th>
+                <th width="25%">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <c:forEach items="${sessionScope.mealList}" var="meal" varStatus="loopCounter">
+                <tr style="color: ${meal.isExceed() ? "red" : "green"}">
+                    <td align="center">
+                        <%=((UserMealWithExceed)pageContext.getAttribute("meal"))
+                            .getDateTime().format(DateTimeFormatter.ofPattern("d MMM yyyy, HH:mm"))
+                        %>
+                    </td>
+                    <td>${meal.getDescription()}</td>
+                    <td align="center">${meal.getCalories()}</td>
+                    <td align="center">
+                        <a href="meal?action=edit&id=${loopCounter.count - 1}" class="pure-button">
+                            Edit
+                        </a>
+                        <a href="meal?action=delete&id=${loopCounter.count - 1}" class="pure-button"
+                                onclick="return confirm('Are you sure you want to delete this meal?')">
+                            Delete
+                        </a>
+                    </td>
+                </tr>
+            </c:forEach>
+        </tbody>
+    </table>
+    <form method="get" action="meal" class="pure-form">
+        <fieldset>
+            <input type="text" placeholder="Date" name="dateTime" value="${sessionScope.editMeal.getDateTime()}">
+            <input type="text" placeholder="Description" name="description" value="${sessionScope.editMeal.getDescription()}">
+            <input type="number" placeholder="Calories" name="calories" value="${sessionScope.editMeal.getCalories()}">
+            <input type="hidden" name="id" value="${sessionScope.id}">
+            <input type="hidden" name="action" value="add">
+            <button type="submit" class="pure-button">Add</button>
+            <a href="meal" class="pure-button">Clear</a>
+        </fieldset>
+    </form>
+    <h2>"<a href="/topjava">Домой"</a></h2>
 </body>
 </html>
