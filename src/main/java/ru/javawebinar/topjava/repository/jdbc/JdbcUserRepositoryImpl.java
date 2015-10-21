1359863962b1e9033711dd972a303803f612d7a8
NicolasUA <nicolas.bakutkin@gmail.com>
2015-10-21 12:01:15 +0300

    Sixth lesson, HW06 Optional

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 /**
  * User: gkislin
@@ -22,7 +27,12 @@ import java.util.List;
 @Repository
 public class JdbcUserRepositoryImpl implements UserRepository {
 
-    private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+    private static final RowMapper<User> ROW_MAPPER = (rs, i) -> {
+    User user = new User(rs.getInt("id"), rs.getString("name"),rs.getString("email"), rs.getString("password"),
+            rs.getInt("calories_per_day"), rs.getBoolean("enabled"), EnumSet.of(Role.valueOf(rs.getString("role"))));
+    user.setRegistered(rs.getDate("registered"));
+    return user;
+};
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -39,6 +49,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
                 .usingGeneratedKeyColumns("id");
     }
 
+    @Transactional
     @Override
     public User save(User user) {
         MapSqlParameterSource map = new MapSqlParameterSource()
@@ -49,14 +60,15 @@ public class JdbcUserRepositoryImpl implements UserRepository {
                 .addValue("registered", user.getRegistered())
                 .addValue("enabled", user.isEnabled())
                 .addValue("caloriesPerDay", user.getCaloriesPerDay());
-
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(map);
             user.setId(newKey.intValue());
+            saveRoles(user.getRoles(), user.getId());
         } else {
             namedParameterJdbcTemplate.update(
                     "UPDATE users SET name=:name, email=:email, password=:password, " +
                             "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", map);
+            saveRoles(user.getRoles(), user.getId());
         }
         return user;
     }
@@ -68,17 +80,36 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        List<User> users = collectRoles(jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id WHERE id=?", ROW_MAPPER, id));
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
-        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
+        List<User> users = collectRoles(jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id WHERE email=?", ROW_MAPPER, email));
+        return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return collectRoles(jdbcTemplate.query("SELECT * FROM users LEFT JOIN user_roles ON users.id = user_roles.user_id ORDER BY name, email", ROW_MAPPER));
+    }
+
+    private void saveRoles(Set<Role> roles, int id) {
+        jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", id);
+        for (Role role : roles) {
+            jdbcTemplate.update("INSERT INTO user_roles (role, user_id) VALUES (?, ?)", role.toString(), id);
+        }
+    }
+
+    private List<User> collectRoles(List<User> users) {
+        for (int i = 1; i < users.size(); i++) {
+            if (users.get(i - 1).getId().equals(users.get(i).getId())) {
+                users.get(i - 1).getRoles().add(users.get(i).getRoles().iterator().next());
+                users.remove(i);
+                i--;
+            }
+        }
+        return users;
     }
 }
--- src/main/webapp/WEB-INF/jsp/fragments/bodyHeader.jsp
+++ src/main/webapp/WEB-INF/jsp/fragments/bodyHeader.jsp
@@ -1,4 +1,10 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 
-<header><a href="/topjava"><fmt:message key="app.title"/></a></header>
+<header>
+    <a href="/topjava"><fmt:message key="app.title"/></a>
+    <span style="float: right">
+        <a href="?lang=en">en</a>
+        <a href="?lang=ru">ru</a>
+    </span>
+</header>
